9957243cfb3dcf47156a7e54d9ec05d36183a6b1
ivan Matyash <ivan.matyash97@gmail.com>
2017-03-26 20:13:16 +0300

    correct code style of vars

--- heap.h
+++ heap.h
@@ -1,26 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-struct heapNode
+
+struct heap_node_struct
 {
 	int priority;
 	unsigned char value;
-	struct heapNode *left;
-	struct heapNode *right;
-	unsigned int huffmanCode;
-	unsigned int amountOfSignificantBits;
-} typedef heapNode;
+	struct heap_node_struct *left;
+	struct heap_node_struct *right;
+	unsigned int huffman_code;
+	unsigned int amount_of_significant_bits;
+} typedef heap_node;
 
 struct heap
 {
-	int maxSize;
-	int curSize;
-	struct heapNode *nodes;
+	int max_size;
+	int cur_size;
+	heap_node *nodes;
 } typedef heap;
 
 heap* create_heap(int);
 void delete_heap();
-void swap_nodes_heap(heapNode*, heapNode*);
-heapNode get_min_node_heap(heap*);
-bool insert_node_heap(heap*, int, unsigned char, heapNode*, heapNode*);
-heapNode remove_min_node_heap(heap*);
+void swap_nodes_heap(heap_node*, heap_node*);
+heap_node get_min_node_heap(heap*);
+bool insert_node_heap(heap*, int, unsigned char, heap_node*, heap_node*);
+heap_node remove_min_node_heap(heap*);
