fdc8557135937535ad1df61b6f8c52f5e1b1ba27
ivan Matyash <ivan.matyash97@gmail.com>
2017-03-26 21:01:38 +0300

    unite compress and decompress in one module

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
+#include <stdio.h>
+#include <string.h>
 #include "encode.h"
+#include "decode.h"
 
-int main(void)
+int main(int argc, char* argv[])
 {
-	encode("file.txt");
+	if (argc != 4) {
+		fprintf(stderr, "%s\n", "Error! Incorrect number of parameters.\nHint: program ifile [-c|-x] ofile");
+		return 1;
+	}
+
+	if (!strcmp(argv[2], "-c")) {
+		encode(argv[1], argv[3]);
+	} else if (!strcmp(argv[2], "-x")) {
+		decode(argv[1], argv[3]);
+	} else {
+		fprintf(stderr, "%s\n", "Error! Wrong arguments.\nHint: program ifile [-c|-x] ofile");
+		return 1;
+	}
+		
 	return 0;
 }
