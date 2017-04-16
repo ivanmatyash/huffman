cba567e189e2618c9fdb76a70b26dbb4a3b3f1ec
Ivan Matyash <ivan.matyash97@gmail.com>
2017-04-16 20:32:56 +0300

    add archivers in test: bzip2, gzip, lha

--- test.sh
+++ test.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-PATH=/usr/bin/time
-PATH_TO_IN_FILE=tests/ifile
+PATH_TO_TIME_FUNC=/usr/bin/time
+PATH_TO_IN_FILE=tests/test.file
 PATH_TO_OUT_FILE=out.bin
 
 n=1
 m=100
 while [ $((i=n++)) -le $m ]
 do
-  $PATH -f "%E" -a --output=time.log ./huff $PATH_TO_IN_FILE -c $PATH_TO_OUT_FILE
+  #$PATH_TO_TIME_FUNC -f "%E" -a --output=huffC.log ./huff $PATH_TO_IN_FILE -c $PATH_TO_OUT_FILE
+  #$PATH_TO_TIME_FUNC -f "%E" -a --output=huffD.log ./huff $PATH_TO_OUT_FILE -x decode.txt
+  #$PATH_TO_TIME_FUNC -f "%E" -a --output=bzip2C.log bzip2 $PATH_TO_IN_FILE
+  #$PATH_TO_TIME_FUNC -f "%E" -a --output=bzip2D.log bzip2 -d $PATH_TO_IN_FILE.bz2
+  $PATH_TO_TIME_FUNC -f "%E" -a --output=gzipC.log gzip $PATH_TO_IN_FILE
+  $PATH_TO_TIME_FUNC -f "%E" -a --output=gzipD.log gunzip $PATH_TO_IN_FILE.gz
 done
